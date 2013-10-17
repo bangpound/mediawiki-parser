968849465dc95adad31dbac20530de056eab2163
Benjamin Doherty <bjd@bangpound.org>
2013-10-16 19:25:03 -0500

    code style.

--- command.php
+++ command.php
@@ -4,7 +4,7 @@
 require 'vendor/autoload.php';
 putenv('MW_INSTALL_PATH=' . __DIR__ . '/vendor/mediawiki/core' );
 $_SERVER['REMOTE_ADDR']='127.0.0.1';
-require('vendor/mediawiki/core/includes/WebStart.php');
+require 'vendor/mediawiki/core/includes/WebStart.php';
 
 use Rshief\Wikimedia\Application;
 
