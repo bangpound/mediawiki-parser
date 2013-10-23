8ed677e7c7dea7a2cafe5742330d5227c291de97
Benjamin Doherty <bjd@bangpound.org>
2013-10-23 11:00:03 -0500

    fix more paths.

--- mediawiki-parser.php
+++ mediawiki-parser.php
@@ -1,14 +1,17 @@
 #!/usr/bin/env php
 <?php
 
+$_SERVER['REMOTE_ADDR']='127.0.0.1';
+
 if (file_exists($a = __DIR__.'/../../autoload.php')) {
     require_once $a;
+    putenv('MW_INSTALL_PATH=' . __DIR__ . '/../../mediawiki/core' );
+    require __DIR__ .'/../../mediawiki/core/includes/WebStart.php';
 } else {
     require_once __DIR__.'/vendor/autoload.php';
+    putenv('MW_INSTALL_PATH=' . __DIR__ . '/vendor/mediawiki/core' );
+    require __DIR__ .'/vendor/mediawiki/core/includes/WebStart.php';
 }
-putenv('MW_INSTALL_PATH=' . __DIR__ . '/vendor/mediawiki/core' );
-$_SERVER['REMOTE_ADDR']='127.0.0.1';
-require 'vendor/mediawiki/core/includes/WebStart.php';
 
 use Rshief\Wikimedia\Application;
 
