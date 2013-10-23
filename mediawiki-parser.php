0aad075226e3e17b6cba88a74389fb64dea7d8ad
Benjamin Doherty <bjd@bangpound.org>
2013-10-23 10:44:59 -0500

    update command to find vendor autoload.php.

--- mediawiki-parser.php
+++ mediawiki-parser.php
@@ -1,7 +1,11 @@
 #!/usr/bin/env php
 <?php
 
-require 'vendor/autoload.php';
+if (file_exists($a = __DIR__.'/../../autoload.php')) {
+    require_once $a;
+} else {
+    require_once __DIR__.'/vendor/autoload.php';
+}
 putenv('MW_INSTALL_PATH=' . __DIR__ . '/vendor/mediawiki/core' );
 $_SERVER['REMOTE_ADDR']='127.0.0.1';
 require 'vendor/mediawiki/core/includes/WebStart.php';
